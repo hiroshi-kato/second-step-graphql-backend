4e242a8168fe1db2bd956070d28ef968416c3152
hiroshi-kato <hiroshikato1208@gmail.com>
2022-02-20 22:58:05 +0900

    NestJS でGraphQLバックエンド - コードファーストアプローチ

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { PostsModule } from './components/posts/posts.module';
+import * as path from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: path.join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    PostsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
